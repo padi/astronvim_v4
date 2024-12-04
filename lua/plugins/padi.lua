b2a8878b20bca935e19164f6c64ad8c9ebdd6cdd
Marc Ignacio <marcrendlignacio@gmail.com>
2024-12-04 22:57:38 +0800

    Add own plugins: nvim-cheat.sh (and popfix)
    nvim-cheat.sh for cheat.sh integration for 
    easily accessing a cheat sheet
    
    and popfix as a dependency for nvim-cheat.sh
    for pop-ups in nvim

---
+++ lua/plugins/padi.lua
@@ -0,0 +1,14 @@
+-- copied from plugins/user.lua,
+-- see file for customization examples
+--
+-- You can also add or configure plugins by creating files in this `plugins/` folder
+-- Here are some examples:
+
+---@type LazySpec
+return {
+
+  -- Add cheat.sh to nvim
+  -- https://github.com/RishabhRD/nvim-cheat.sh?tab=readme-ov-file#installation
+  "RishabhRD/popfix",
+  "RishabhRD/nvim-cheat.sh",
+}
